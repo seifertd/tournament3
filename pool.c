d77dbdca154ed925e270b5a38f569ac98210813e
Douglas A. Seifert <doug@dseifert.net>
2023-03-27 07:19:23 -0700

    Implement -d arg

--- pool.c
+++ pool.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 #define POOL_IMPLEMENTATION
 #include "pool.h"
 
-int main(int argc, const char **argv) {
+void usage(char *progName) {
+  fprintf(stderr, "Usage: %s -d DIR\n", progName);
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+  int opt;
+  bool initialized = false;
+  while ((opt = getopt(argc, argv, "d:")) != -1) {
+    switch (opt) {
+      case 'd':
+        pool_initialize(optarg);
+        initialized = true;
+        break;
+      default:
+        usage(argv[0]);
+    }
+  }
+  if (!initialized) {
+    usage(argv[0]);
+  }
+  pool_team_report();
+  pool_score_report();
   return 0;
 }
